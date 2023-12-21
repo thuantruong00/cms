293beaec04dac770414850a1b6e4df6a2ea23622
minh832000 <18521111@gm.uit.edu.vn>
2023-12-21 15:43:19 +0700

    inserting data for user table

---
+++ scripts/users.script.ts
@@ -0,0 +1,26 @@
+import { PrismaClient } from '@prisma/client';
+
+import users from './users.fake.json';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  for (let user of users) {
+    let { id, ...rest } = user;
+    await prisma.user.create({
+      data: {
+        ...rest,
+      },
+    });
+  }
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
